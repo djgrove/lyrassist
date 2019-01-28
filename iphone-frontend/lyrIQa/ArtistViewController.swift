bfd570764fec6adcf183f264d2ddcc709b9db6f7
Austin McInnis <austin.mcinnis@icloud.com>
2019-01-27 23:17:43 -0800

    Decoding JSON. Displaying artists and song data. Need to reformat JSON structure.

---
+++ iphone-frontend/lyrIQa/ArtistViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ArtistViewController.swift
+//  lyrIQa
+//
+//  Created by Austin McInnis on 1/27/19.
+//  Copyright © 2019 Austin McInnis. All rights reserved.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController {
+
+    
+    @IBOutlet var songTextView: UITextView!
+    var artist:Artist?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let artist = artist {
+            self.navigationItem.title = artist.name
+            self.songTextView.text = artist.data
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
