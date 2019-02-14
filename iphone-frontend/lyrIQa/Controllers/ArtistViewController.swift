ee4d92d3016d477151767283256bc21b8c30afc8
Austin McInnis <austin.mcinnis@icloud.com>
2019-02-14 14:22:18 -0800

    Reorganized xcode project. Added custom lyrAssistNavController to handle custom color scheme. Inital stages of downloading artist images.

--- iphone-frontend/lyrIQa/Controllers/ArtistViewController.swift
+++ iphone-frontend/lyrIQa/Controllers/ArtistViewController.swift
@@ -9,22 +9,51 @@
 import UIKit
 
 protocol ArtistDelegate {
-    func updateArtist(id: Int, withSong: String)
+    func updateArtistSong(id: Int, withSong: String)
+    func updateArtistImage(id: Int, withImage: UIImage)
 }
 
 class ArtistViewController: UIViewController {
 
-    
     @IBOutlet var songTextView: UITextView!
+    
     var artist:Artist?
     var artistDelegate: ArtistDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        //Setup info button
+        setupInfoButton()
         if let artist = artist {
             self.navigationItem.title = artist.name
+            if artist.avatarImage != nil {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+            }
+            else if let avatarURLStr = artist.avatarURL, let avatarURL = URL(string: avatarURLStr) {
+                let avatarTask = URLSession.shared.dataTask(with: avatarURL) {
+                    [weak self] data, response, error in
+                    guard error == nil else {
+                        print(error!.localizedDescription)
+                        return
+                    }
+                    guard let this = self else { return }
+                    if let data = data, let image = UIImage(data: data) {
+                        //Update Data Model
+                        this.artist?.avatarImage = image
+                        this.artistDelegate?.updateArtistImage(id: artist.id!, withImage: image)
+                        
+                        //Update Nav Bar UI for info button
+                        DispatchQueue.main.async {
+                            this.navigationItem.rightBarButtonItem?.isEnabled = true
+                        }
+                    }
+                }
+                avatarTask.resume()
+            }
+            
             if let song = artist.song {
                 self.songTextView.text = song
             }
@@ -32,7 +61,7 @@ class ArtistViewController: UIViewController {
                 //Query, use delegation to store song in artist object
                 // TODO: Setup delegation to store song in cache
                 if let urlStr = artist.url, let url = URL(string: urlStr) {
-                    let task = URLSession.shared.dataTask(with: url) {
+                    let songTask = URLSession.shared.dataTask(with: url) {
                         [weak self] data, response, error in
                         guard let this = self else { return }
                         if let data = data{
@@ -43,7 +72,7 @@ class ArtistViewController: UIViewController {
                                         DispatchQueue.main.async {
                                             this.songTextView.text = song
                                         }
-                                        this.artistDelegate?.updateArtist(id: artist.id!, withSong: song)
+                                        this.artistDelegate?.updateArtistSong(id: artist.id!, withSong: song)
                                     }
                                     else {
 //                                        preconditionFailure("Could not get song from json for \(artist.name)")
@@ -58,24 +87,25 @@ class ArtistViewController: UIViewController {
                             this.songTextView.text = error?.localizedDescription
                         }
                     }
-                    task.resume()
+                    songTask.resume()
                 }
                 else {
                     fatalError("Received invalid url for artist: \(artist.name)")
                 }
             }
         }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupInfoButton() {
+        let infoButton = UIButton(type: .infoLight)
+        infoButton.addTarget(self, action: #selector(infoButtonTapped), for: .touchUpInside)
+        let barButton = UIBarButtonItem(customView: infoButton)
+        barButton.isEnabled = false
+        self.navigationItem.rightBarButtonItem = barButton
     }
-    */
-
+    
+    @objc func infoButtonTapped() {
+        print("Info button tapped")
+    }
 }
---
+++ iphone-frontend/lyrIQa/Models/Artist.swift
@@ -0,0 +1,29 @@
+//
+//  Artist.swift
+//  lyrIQa
+//
+//  Created by Austin McInnis on 2/14/19.
+//  Copyright © 2019 Austin McInnis. All rights reserved.
+//
+import UIKit
+import Foundation
+
+struct Artist: Codable {
+    var id: Int?
+    var name: String
+    var urlName: String
+    var url: String?
+    var avatarURL: String?
+    var avatarImage: UIImage?
+    var song: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, url, song, urlName
+        case avatarURL = "avatar"
+    }
+}
+
+struct ArtistListResponse: Codable {
+    var statusCode: Int
+    var data: [Artist]
+}
