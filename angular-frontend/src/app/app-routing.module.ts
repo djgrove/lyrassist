fb543efa5e9d62e92645cee2b44e18723731a42e
djgrove <devon.j.grove@gmail.com>
2019-02-14 11:13:18 -0800

    Rebranded Troubaidor->LyrAssist

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
-import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'create/:artist', component: CreateComponent, pathMatch: 'full'},
-  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
