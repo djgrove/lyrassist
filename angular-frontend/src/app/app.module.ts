3e5d90d601d7157147dfac3e6f835673fa7053b1
djgrove <devon.j.grove@gmail.com>
2019-02-14 11:13:18 -0800

    Rebranded Troubaidor->LyrAssist

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -6,16 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { CreateComponent } from './create/create.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    ContactComponent,
-    CreateComponent
+    CreateComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
