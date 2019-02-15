169851fdff0249ef96ac47f0d6b67a4713fcac93
Austin McInnis <austin.mcinnis@icloud.com>
2019-02-14 16:44:50 -0800

    Restructured networking function calls in viewDidLoad() for better readability.

--- iphone-frontend/lyrIQa/Controllers/ArtistViewController.swift
+++ iphone-frontend/lyrIQa/Controllers/ArtistViewController.swift
@@ -24,70 +24,23 @@ class ArtistViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        
-        //Setup info button
         setupInfoButton()
+        
         if let artist = artist {
             self.navigationItem.title = artist.name
             if artist.avatarImage != nil {
                 self.navigationItem.rightBarButtonItem?.isEnabled = true
             }
             else if let avatarURLStr = artist.avatarURL, let avatarURL = URL(string: avatarURLStr) {
-                let avatarTask = URLSession.shared.dataTask(with: avatarURL) {
-                    [weak self] data, response, error in
-                    guard error == nil else {
-                        print(error!.localizedDescription)
-                        return
-                    }
-                    guard let this = self else { return }
-                    if let data = data, let image = UIImage(data: data) {
-                        //Update Data Model
-                        this.artist?.avatarImage = image
-                        this.artistDelegate?.updateArtistImage(id: artist.id!, withImage: image)
-                        
-                        //Update Nav Bar UI for info button
-                        DispatchQueue.main.async {
-                            this.navigationItem.rightBarButtonItem?.isEnabled = true
-                        }
-                    }
-                }
-                avatarTask.resume()
+                downloadArtistImage(url: avatarURL)
             }
             
             if let song = artist.song {
                 self.songTextView.text = song
             }
             else {
-                //Query, use delegation to store song in artist object
-                // TODO: Setup delegation to store song in cache
                 if let urlStr = artist.url, let url = URL(string: urlStr) {
-                    let songTask = URLSession.shared.dataTask(with: url) {
-                        [weak self] data, response, error in
-                        guard let this = self else { return }
-                        if let data = data{
-                            do {
-                            let json = try JSONSerialization.jsonObject(with: data, options: [])
-                                if let jsonData = json as? [String:Any] {
-                                    if let song = jsonData["data"] as? String {
-                                        DispatchQueue.main.async {
-                                            this.songTextView.text = song
-                                        }
-                                        this.artistDelegate?.updateArtistSong(id: artist.id!, withSong: song)
-                                    }
-                                    else {
-//                                        preconditionFailure("Could not get song from json for \(artist.name)")
-                                    }
-                                }
-                            }
-                            catch let jsonError {
-                                fatalError(jsonError.localizedDescription)
-                            }
-                        }
-                        else {
-                            this.songTextView.text = error?.localizedDescription
-                        }
-                    }
-                    songTask.resume()
+                    downloadArtistSong(url: url)
                 }
                 else {
                     fatalError("Received invalid url for artist: \(artist.name)")
@@ -95,6 +48,58 @@ class ArtistViewController: UIViewController {
             }
         }
         
+    }
+    
+    func downloadArtistImage(url: URL) {
+        let avatarTask = URLSession.shared.dataTask(with: url) {
+            [weak self] data, response, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let this = self else { return }
+            if let data = data, let image = UIImage(data: data) {
+                //Update Data Model
+                this.artist?.avatarImage = image
+                this.artistDelegate?.updateArtistImage(id: this.artist!.id!, withImage: image)
+                
+                //Update Nav Bar UI for info button
+                DispatchQueue.main.async {
+                    this.navigationItem.rightBarButtonItem?.isEnabled = true
+                }
+            }
+        }
+        avatarTask.resume()
+    }
+    
+    func downloadArtistSong(url: URL) {
+        let songTask = URLSession.shared.dataTask(with: url) {
+            [weak self] data, response, error in
+            guard let this = self else { return }
+            if let data = data{
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let jsonData = json as? [String:Any] {
+                        if let song = jsonData["data"] as? String {
+                            DispatchQueue.main.async {
+                                this.songTextView.text = song
+                            }
+                            this.artistDelegate?.updateArtistSong(id: this.artist!.id!, withSong: song)
+                        }
+                        else {
+//                            preconditionFailure("Could not get song from json for \(artist.name)")
+                        }
+                    }
+                }
+                catch let jsonError {
+                    fatalError(jsonError.localizedDescription)
+                }
+            }
+            else {
+                this.songTextView.text = error?.localizedDescription
+            }
+        }
+        songTask.resume()
     }
     
     func setupInfoButton() {
