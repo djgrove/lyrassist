24a1e8be3c3726d0dc5a74ad995a3657ced4a219
Austin McInnis <austin.mcinnis@icloud.com>
2019-02-14 16:23:23 -0800

    Added Custom UIColor extension for lyrAssist color scheme. Now presenting bio view controller modally.

---
+++ iphone-frontend/lyrIQa/Style/lyrAssistColorScheme.swift
@@ -0,0 +1,14 @@
+//
+//  lyrAssistColorScheme.swift
+//  lyrIQa
+//
+//  Created by Austin McInnis on 2/14/19.
+//  Copyright © 2019 Austin McInnis. All rights reserved.
+//
+import UIKit
+import Foundation
+
+extension UIColor {
+    static let lyrBurgundy = UIColor(red: 127.0/255.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    static let lyrLightRed = UIColor(red: 148.0/255.0, green: 44.0/255.0, blue: 44.0/255.0, alpha: 1.0)
+}
