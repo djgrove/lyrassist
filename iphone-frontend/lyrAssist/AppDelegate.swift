cc13f751bf05d3bc11d6caed09bc5b023da67b9e
Austin McInnis <austin.mcinnis@icloud.com>
2019-03-27 12:48:07 -0700

    Added Firebase to project. Initial Auth commit.

--- iphone-frontend/lyrAssist/AppDelegate.swift
+++ iphone-frontend/lyrAssist/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Austin McInnis. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
